5b59832ea2f11bf0134520c1814e3342e8b4e8d7
GabrielDwiki-github <rp755404@gmail.com>
2024-09-24 16:29:46 +0700

    coding hello world

---
+++ ARRAY.java
@@ -0,0 +1,5 @@
+class coba {
+  public static void main(String[] args) {
+    String[] motor = {"Honda", "Kawasaki","Aerox"};
+    System.out.println(motor[2]);
+}}
---
+++ ProgramDira.java
@@ -0,0 +1,7 @@
+public class ProgramDira {
+  public static void main(String[] args) {
+      System.out.print("Hellow");
+      System.out.print("world");
+      
+  }
+}
